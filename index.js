c5d8f6e5cca7d08030eb014b1c1c8b58607216a2
charles peters <charlespeters42@gmail.com>
2016-09-02 07:50:11 -0700

    scoping import and variables

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-import axios from 'axios'
+import { get } from 'axios'
 
 const API = 'https://randoma11y.com/stats/'
-const fallback = {
-  color_one: '#001F3F',
-  color_two: '#FF4136'
-}
 
 class Colors {
   constructor (x) {
@@ -30,7 +26,7 @@ class Colors {
   // }
 
   random (x) {
-    return axios.get(x).then((r) => {
+    return get(x).then((r) => {
       const data = r.data.most_active_20
       const i = Math.floor(Math.random() * 20)
 
@@ -43,6 +39,11 @@ class Colors {
 
       this.style(clrv.color_one, clrv.color_two)
     }).catch((err) => {
+      const fallback = {
+        color_one: '#001F3F',
+        color_two: '#FF4136'
+      }
+
       console.warn(`${err}: API is down`)
 
       this.style(fallback.color_one, fallback.color_two)
